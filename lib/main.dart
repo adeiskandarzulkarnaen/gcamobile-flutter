6781132afa03225a7348d46a163bbabf1363c3d2
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2024-05-07 17:33:08 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:gcamobile/live_stream_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GCA Mobile',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      home: const LiveStreamScreen(),
+    );
+  }
+}
