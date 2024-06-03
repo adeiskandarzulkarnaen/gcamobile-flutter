80a397e634a0d53bd4f68ab2ea54c75735e6e012
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2024-06-03 08:31:31 +0700

    add cctv_list example

---
+++ lib/cctv_list.dart
@@ -0,0 +1,28 @@
+import 'package:gcamobile/models/cctv.dart';
+
+List<Cctv> cctvList = [
+  Cctv(
+    name: "Perempatan Ciwalen (Asia)",
+    location: "Jl. Ahmad Yani, Ciwalen, Kec. Garut Kota, Kabupaten Garut, Jawa Barat 44116",
+    latitude: "-7.215947872294545",
+    longitude: "107.90888932477445",
+    thumbnail: "https://garutkab.go.id/thumbnail/",
+    linkRtmp: "https://cctv.garutkab.go.id/r7c97OBE4VCf/mp4/diskominfo/NkR1JvftY0/s.mp4",
+  ),
+  Cctv(
+    name: "Bunderan Suci",
+    location: "Bunderan Suci Karangpawitan Garut",
+    latitude: "-7.215913094001971",
+    longitude: "107.92116880361597",
+    thumbnail: "https://garutkab.go.id/thumbnail/",
+    linkRtmp: "https://cctv.garutkab.go.id/r7c97OBE4VCf/mp4/diskominfo/ZWZaBMMtet/s.mp4",
+  ),
+  Cctv(
+    name: "Bunderan Leuwi Daun",
+    location: "Jl. Cimanuk, Bunderan Leuwi Daun",
+    latitude: "",
+    longitude: "",
+    thumbnail: "https://garutkab.go.id/thumbnail/",
+    linkRtmp: "https://cctv.garutkab.go.id/r7c97OBE4VCf/mp4/diskominfo/leuwidaun/s.mp4",
+  ),
+];
